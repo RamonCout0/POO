894e91a84405c14d67acfe393064bff5586d31ac
Ramon Couto <ramon.couto08.s@gmail.com>
2025-04-26 20:36:54 +0000

    atividade

---
+++ formula/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main{
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US); // isso é para colocar um ponto decimal
+        Scanner sc = new Scanner(System.in);
+
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        double C = sc.nextDouble();
+
+        double delta = B * B - 4 * A * C;
+
+        if(A == 0 || delta < 0){
+            System.out.println("Impossivel calcular");
+            
+        }else{
+            double R1 = (-B + Math.sqrt(delta)) / (2 * A);
+            double R2 = (-B - Math.sqrt(delta)) / (2 * A);
+            
+            System.out.printf("R1 = %.5f\n",R1);
+            System.out.printf("R2 = %.5f\n", R2);
+        }
+
+        sc.close();
+    }
+}
