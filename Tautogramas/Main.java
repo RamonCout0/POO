e5cf7db2ecea7a844557923c965ddaeefa157217
Ramon Couto <ramon.couto08.s@gmail.com>
2025-04-30 17:32:13 +0000

    mds que atv é esssa

---
+++ Tautogramas/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main{
+    public static void main (String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        
+        char palavra(1); 
+        System.out.println("Escreva uma frase");
+        String n = sc.nextInt();
+
+
+        }
+
+     
+
+        
+
+
+        sc.close();
+    }
+}
+
+
+
+/** Agora que eu entendi o que é Tautograma, é um frase 
+ * que consiste na primeira letra se repete.
+ * Basicamente tenho que criar um programa e checar se ele é um
+ * tautograma ou não. a palavra tem que ter 20 letras
+ * e a frase 50 tanto faz sendo maiuscula ou não.
+ * 
+ */
