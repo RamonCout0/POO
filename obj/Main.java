11910da2fc8595ac676b8b1e71c2732484ccce7d
Ramon Couto <ramon.couto08.s@gmail.com>
2025-05-14 17:21:51 +0000

    java

---
+++ obj/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main{
+
+class Retangulo{
+    private double altura, base;
+
+    public Retangulo(double altura, double base){
+    this.base = base;
+    this.altura = altura;
+        }
+    }
+
+    public double getBase(){
+        return base;
+    }
+    public void setBase(){
+        this.base = base;
+    }
+}
+
+
+
+
+
+
+
+
