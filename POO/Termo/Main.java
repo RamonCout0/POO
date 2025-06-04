f251ebb69ff9938294130f35fb303221861e802d
Ramon Couto <ramon.couto08.s@gmail.com>
2025-06-04 17:06:31 +0000

    mds

---
+++ POO/Termo/Main.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+
+
+
+public class main{
+
+public checar(){
+   int j;
+   int i; 
+
+}
+
+public LerPalavras(){
+    Path LerPalavras = Paths.get("/POO/Termo/palavras.txt");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static void main(String[] args) {
+    
+
+}
+
+
+
+
+
+
+
+
+
+}
