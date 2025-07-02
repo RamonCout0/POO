b57c84a00bc96b43bf724c63a7aefc3db003c920
Ramon Couto <ramon.couto08.s@gmail.com>
2025-07-02 17:22:15 +0000

    teste

---
+++ abs/Retangulo.java
@@ -0,0 +1,19 @@
+public abstract class Retangulo{
+    private double b;
+    private double h;
+
+    public Retangulo(double b, double h){
+    this.b = b;
+    this.h = h;
+    this
+}
+    public double GetBase(){
+        return GetBase();
+    }
+    public double GetAltura(){
+    return 0;      
+    }
+    public double
+};
+
+
